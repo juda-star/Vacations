6ad517f70f9897d18179adddadebb1510a297f22
juda-star <juda.ishta@gmail.com>
2021-07-30 22:21:29 +0300

    form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Contact() {
     return (
         <div>
-            
+            <form action="">
+                
+            </form>
         </div>
     )
 }
