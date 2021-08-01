32f07ef3806269d5c4815ede459500b5b59db3ec
juda-star <juda.ishta@gmail.com>
2021-08-01 23:19:25 +0300

    container

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -4,6 +4,8 @@ export default function Container() {
   return (
     <div>
       <h2>welcome to vacation web</h2>
+      <a href="http://" target="_blank">video</a>
+      <a href="http://" target="_blank">imges</a>
     </div>
   );
 }
