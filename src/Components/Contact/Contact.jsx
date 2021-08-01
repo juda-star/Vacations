c46d397ba05d999ac9631e72478c9f5a0901519d
juda-star <juda.ishta@gmail.com>
2021-08-01 23:36:50 +0300

    form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,9 +3,21 @@ import React from 'react'
 export default function Contact() {
     return (
         <div>
-            <form action="">
-                
-            </form>
+          <form action="">
+              <label htmlFor="">First Name
+                  <input type="text" />
+              </label>
+              <label htmlFor="">Last Name
+                  <input type="text" />
+              </label>
+              <label htmlFor="">Age
+                  <input type="text" />
+              </label>
+              <label htmlFor="">Email
+                  <input type="text" />
+              </label>
+
+          </form>
         </div>
     )
 }
