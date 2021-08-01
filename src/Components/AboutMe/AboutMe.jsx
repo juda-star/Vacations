0ced59a611c8976cc5f8926b19c41d62518c7cf1
juda-star <juda.ishta@gmail.com>
2021-08-01 23:15:40 +0300

    aboutme

---
+++ src/Components/AboutMe/AboutMe.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>hii I am Yehooda</h1>
+      <p>i wont to tell you how mach i like to traval</p>
+    </div>
+  );
+}
